29038593a6f8ad7e57a121719f0aa66fa33717ff
gutenLEE <3bnm1128@naver.com>
2022-10-07 21:39:42 +0900

    :tada: start!!!!!

---
+++ src/main/java/com/example/owlcheckin/OwlCheckInApplication.java
@@ -0,0 +1,13 @@
+package com.example.owlcheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlCheckInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlCheckInApplication.class, args);
+	}
+
+}
